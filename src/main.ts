f58478dbe604fde2f73d1ccf53248d4a09dd9909
ibrahimabdalrhman <ibrahimabdalr7man@gmail.com>
2024-05-16 00:37:16 +0300

    Custom interceptor to add data as a key in response data

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { WrapDataInterceptor } from './common/interceptors/wrap-data/wrap-data.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, forbidNonWhitelisted: true,
     transform: true,
-    transformOptions: {enableImplicitConversion:true}
+    transformOptions: { enableImplicitConversion: true }
 
   }));
+  app.useGlobalInterceptors(new WrapDataInterceptor());
   await app.listen(3000);
 }
 bootstrap();
