46320b01aeae2edc6a75086012ae227e35f685e1
ibrahimabdalrhman <ibrahimabdalr7man@gmail.com>
2024-05-16 13:36:10 +0300

    custom timeout Interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { WrapDataInterceptor } from './common/interceptors/wrap-data/wrap-data.interceptor';
+import { TimeoutInterceptor } from './common/interceptors/timeout/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +12,7 @@ async function bootstrap() {
     transformOptions: { enableImplicitConversion: true }
 
   }));
-  app.useGlobalInterceptors(new WrapDataInterceptor());
+  app.useGlobalInterceptors(new WrapDataInterceptor(),new TimeoutInterceptor());
   await app.listen(3000);
 }
 bootstrap();
